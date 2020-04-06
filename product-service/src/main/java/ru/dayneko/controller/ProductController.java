97f7020879fdb52fd957bfca67d8f5530137e5a1
sdayneko <sergeydayneko@mail.ru>
2020-04-06 11:10:53 +0300

    configured base webflux config

---
+++ product-service/src/main/java/ru/dayneko/controller/ProductController.java
@@ -0,0 +1,14 @@
+package ru.dayneko.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/product")
+    public Mono<String> getProduct() {
+        return Mono.justOrEmpty("Hello");
+    }
+}
