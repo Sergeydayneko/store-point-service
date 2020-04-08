6caf3634f9f17825b8ce122ddcff7c2752305ed8
sdayneko <sergeydayneko@mail.ru>
2020-04-08 12:29:38 +0300

    added actuator for further refresh configs

--- product-service/src/main/java/ru/dayneko/controller/ProductController.java
+++ product-service/src/main/java/ru/dayneko/controller/ProductController.java
@@ -1,8 +1,10 @@
 package ru.dayneko.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
 
 @RestController
 public class ProductController {
