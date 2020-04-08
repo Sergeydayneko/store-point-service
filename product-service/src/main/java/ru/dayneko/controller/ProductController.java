1efea16d1a4183df4422f6fd15c52edc20cdda55
sdayneko <sergeydayneko@mail.ru>
2020-04-08 12:31:32 +0300

    removed extra import

--- product-service/src/main/java/ru/dayneko/controller/ProductController.java
+++ product-service/src/main/java/ru/dayneko/controller/ProductController.java
@@ -1,6 +1,5 @@
 package ru.dayneko.controller;
 
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
